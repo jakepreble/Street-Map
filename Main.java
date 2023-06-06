868c222f1a13803777099c3fed726508cb200e21
jakepreble <jakepreble3@gmail.com>
2023-06-06 18:35:11 -0400

    grrgsef

--- Main.java
+++ Main.java
@@ -7,23 +7,23 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, RateLimitException {
-        //remember to fill in your own assignment number, username, and api key
-        Bridges bridges = new Bridges(1, "jpreble", "250600616660");
+        // remember to fill in your own assignment number, username, and api key
+        Bridges bridges = new Bridges(2, "jpreble", "250600616660");
         bridges.setTitle("Shortest path");
         bridges.setDescription("Shortest path across New York City (created with data from OpenStreetMaps)");
-       
-        //get NYC map from bridges data server
+
+        // get NYC map from bridges data server
         DataSource ds = bridges.getDataSource();
         OsmData data = ds.getOsmData("New York, New York", "secondary");
-
-        //find shortest path from northwest to southeast
+        System.out.print("test0");
+        // find shortest path from northwest to southeast
         StreetMap map = new StreetMap(data);
-        //System.out.println(map.outerVertex(1, -1));
-        
-        map.shortestPath(map.outerVertex(1, -1), map.outerVertex(-1, 1), new Color("red"));
+        // System.out.println(map.outerVertex(1, -1));
+        System.out.print("test1");
+        //map.shortestPath(map.outerVertex(1, -1), map.outerVertex(-1, 1), new Color("red"));
         bridges.setDataStructure(map);
         bridges.visualize();
-        System.out.print("test");
-        
+        System.out.print("test2");
+
     }
 }
